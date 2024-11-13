ec7a4a70cc3bb2cfb36ead5551d32405971c2038
minecraft-beta <ronicyt69@gmail.com>
2024-11-13 15:09:56 +0530

    menu logic

--- filmvibe/script.js
+++ filmvibe/script.js
@@ -1,6 +1,11 @@
 const burger = document.getElementById('burger');
 const menuPage = document.getElementById('menuPage');
 
-burger.onclick = function () {
+burger.addEventListener('change', function() {
+    if (burger.checked) {
     menuPage.style.display = 'block';
-}
+    } else if (!burger.checked) {
+        menuPage.style.display = 'none';
+    }
+});
+

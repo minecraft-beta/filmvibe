1f9ff642e013dcb0d77411a390c5e84f6ccee5e2
minecraft-beta <ronicyt@gmail.com>
2024-11-22 09:27:42 +0000

    ngrok

---
+++ ngrok.py
@@ -0,0 +1,13 @@
+from pyngrok import ngrok
+import os
+
+ngrok.set_auth_token("2brFtV6cyUE8RulqFo4azDVxUKz_2YRVPrug7ufwchpiRToMd")
+
+# Set the port for your server
+port = 6969
+
+# Open a tunnel on the desired port
+public_url = ngrok.connect(port)
+print('Public URL:', public_url)
+
+os.system("node server.js")
